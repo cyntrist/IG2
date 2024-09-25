e1dffdbfce9a6ebf968413d40fe869f2af2606a3
ineprimo <ineprimo@ucm.es>
2024-09-25 12:49:46 +0200

    ehhh ehhh joder no joder no no lo see hehehe ineprimo no puedo ineprimo inecousin no puedo todo eso

--- IG2App/Heroe.cpp
+++ IG2App/Heroe.cpp
@@ -1,1 +1,23 @@
 #include "Heroe.h"
+
+void Heroe::keypressed(const OgreBites::KeyboardEvent& evt)
+{
+	if (evt.keysym.sym == SDLK_UP) {
+		
+	}
+	else if (evt.keysym.sym == SDLK_RIGHT) {
+
+	}	
+	else if (evt.keysym.sym == SDLK_DOWN) {
+
+	}	
+	else if (evt.keysym.sym == SDLK_LEFT) {
+
+	}
+
+}
+
+void Heroe::addPoint(int i)
+{
+	puntos += i;
+}
--- IG2App/Heroe.h
+++ IG2App/Heroe.h
@@ -4,14 +4,75 @@
 
 using Chelo = Heroe;
 
+
+/// TO DO LIST:
+///
+/// 1. input (OgreBites::Keycode)
+/// 2. leerlo en el parseo (on hold)
+/// 3. listener
+/// 4. colisiones
+
 // clase heroe (chelo) 
 class Heroe : public IG2Object
 {
+private:
+
+	// puntos y vidas
+	int vidas;
+	int puntos;
+	
+	// direccion actual
+	Vector3 dir;
+	// direccion nueva
+	Vector3 newdir;
+
+public:
+
+	// constructora basica, genera el objeto en el origen
+	Heroe() : IG2Object() {
+		vidas = 3;
+		puntos = 0;
+	};
+
+	// constructora
+	Heroe(Vector3 initPos, SceneNode* node, SceneManager* sceneMng, String mesh)
+		: IG2Object(initPos, node, sceneMng, mesh) {
+	
+		vidas = 3;
+		puntos = 0;
+	}
+
+	Heroe(Vector3 initPos, SceneNode* node, SceneManager* sceneMng, String mesh, int vidas_, int puntos_)
+		: IG2Object(initPos, node, sceneMng, mesh), vidas(vidas_), puntos(puntos_) { }
 
 
+// ------------------- METODOS ------------------------
 
+	// 
+	// addInputListener()
+	void keypressed(const OgreBites::KeyboardEvent& evt);
 
+	// ---------- MOVIMIENTO -------------
+	//
+	void updateMovement(Vector3 vec);
 
+	// de momento solo en un eje
+	// mDragonNode->yaw(Degree(-180));
+	void updateRotation(int degree);
+
+	// devuelve si se puede mover en esa direccion (si no hay muro con el que colisiona)
+	bool canMove(Vector3 vec);
+
+	// detecta las colisiones
+	bool checkCollision();
+
+	// come una perla
+	void eatPearl();
+
+	// añade puntos al score
+	void addPoint(int i);
+
+	//
 
 
 };
