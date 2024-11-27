90e1bcd18c7eef8a36f4f26bb3d7408b7839ac8d
ineprimo <ineprimo@ucm.es>
2024-11-27 12:52:23 +0100

    ayuda

--- practica 2/media/IG2App/SpaceSkyShaderFS.glsl
+++ practica 2/media/IG2App/SpaceSkyShaderFS.glsl
@@ -1,13 +1,18 @@
 #version 330 core
 in vec2 vUv0;
+// meter un vUv1
+
 uniform sampler2D texturaL;
 uniform sampler2D texturaM;
 uniform float BF;
 uniform float intLuzAmb;
+
+// profundidad tmb -> gl_FragDepth
 out vec4 fFragColor;
+
  void main() { 
-    //vec3 colorL = vec3(texture(texturaL, vUv0));
+    vec3 colorL = vec3(texture(texturaL, vUv0));
 	//vec3 colorM = vec3(texture(texturaM, vUv0));
- 	//vec3 color = mix(colorL, colorM, BF) * intLuzAmb;
- 	//fFragColor = vec4(color, 1.0);
+ 	vec3 color = colorL; //*colorM;
+ 	fFragColor = vec4(color, 1.0);
  }
--- practica 2/media/IG2App/SpaceSkyShaderVS.glsl
+++ practica 2/media/IG2App/SpaceSkyShaderVS.glsl
@@ -5,16 +5,12 @@ in vec4 vertex;
 in vec2 uv0;
 
 uniform mat4 modelViewProjMat;
+
 out vec2 vUv0;
 
+
  void main() {
- 	//vUv0 = uv0;
 
-    // centrar en el cuadrado           (traslacion)
-    // aplicar el facor zoom (ZF)       (escala)
-    // centrarlas en el nuevo cuadrado  (traslacion)
-
-    //vUv0.s = (uv0.s- 0.5) * (ZF) + 0.5; 
-    //vUv0.t = (uv0.t- 0.5) * (ZF) + 0.5;
- 	//gl_Position = modelViewProjMat * vertex;
+   vUv0=uv0; 
+   gl_Position = modelViewProjMat*vertex;
  }
