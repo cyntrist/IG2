e3681601668a042506bbec494e56be1bc2783acd
ineprimo <ineprimo@ucm.es>
2024-11-30 17:21:10 +0100

    4.1 hecho?????????????????????????????
    toca hacer la esfera
    
    que miedo

--- practica 2/media/IG2App/SpaceSkyShaderVS.glsl
+++ practica 2/media/IG2App/SpaceSkyShaderVS.glsl
@@ -2,23 +2,23 @@
 
 in vec4 vertex;
 uniform mat4 modelViewProjMat;
-uniform float my_time;
+uniform float time;
 uniform float minZoom; 
 uniform float maxZoom; 
 
 in vec2 uv0;
-out vec2 textCoord;
+out vec2 vUv0;
 
 void main() {   
 
     float a = (minZoom + maxZoom)/2;
-    float b = maxZoom  -a;
+    float b = maxZoom - a;
 
-    //formula buena : (my_time*b) + a
-    float factor = (my_time*b) + a; //zoom factor
+	// ZF = sintime * a + b
+    float factor = (time*b) + a; //zoom factor
 
-    textCoord.s = (uv0.s- minZoom) * (factor) + minZoom;
-    textCoord.t = (uv0.t- minZoom) * (factor) + minZoom;
+    vUv0.s = (uv0.s - minZoom) * (factor) + minZoom;
+    vUv0.t = (uv0.t - minZoom) * (factor) + minZoom;
     
     gl_Position = modelViewProjMat * vertex;  
 
