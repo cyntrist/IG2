9cd4ae6654ac2e4501f09bcfdcf7ffce3be34b11
ineprimo <ineprimo@ucm.es>
2024-12-16 19:02:33 +0100

    checkpointtttttttt

--- practica 2/media/IG2App/practice_shaderFS.glsl
+++ practica 2/media/IG2App/practice_shaderFS.glsl
@@ -3,9 +3,22 @@
 in vec2 vUv0; 
 uniform sampler2D textCorrosion;  
 uniform sampler2D textChelo;  
+uniform sampler2D textMetal;  
 
 out vec4 fFragColor; 
 
 void main() {   
-    fFragColor=vec4(1, 0.5, 0.2,1.0);
+
+    vec3 check_color = texture(textCorrosion, vUv0).rgb;
+    bool check = check_color.r == 0 && check_color.g == 0 && check_color.b == 0;
+
+    vec4 color = texture(textChelo, vUv0);
+    if(check){
+        vec4 color = texture(textChelo, vUv0);
+    }
+    else{
+        vec4 color = texture(textMetal, vUv0);
+    }
+
+    fFragColor=color;
 } 
